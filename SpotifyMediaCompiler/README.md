d96ba6050dca22d084c8e92cd78f8d0c7dde430a
sirnoob_1 <102556049+sirnoob-1@users.noreply.github.com>
2022-10-06 23:48:25 +0100

    Initial Commit

--- SpotifyMediaCompiler/README.md
+++ SpotifyMediaCompiler/README.md
@@ -1,1 +1,35 @@
+# Getting Started
 
+### The Spotify Media Compiler is designed for Azuracast or Icecast supporting servers only, and can be ran in multiple ways, I would recommend knowledge on how you are going to implement it before continuing. For this example, let's keep it simple.
+
+---
+## Installation Brief
+```
+Step 1) Download the SpotifyMediaCompiler.py file
+Step 2) Place the file in a clear directory
+Step 3) Edit variables with corresponding data
+Step 4) Running the file
+```
+---
+## Step 1) Downloading
+### To download the file, head to the github page and click 'Download'
+--
+## Step 2) Moving
+### Make sure the file is out of any busy directories, moving it to an empty folder will help to improve the performance
+--
+## Step 3) Editing Variables
+### In order for the file to run, you must edit the corresponding data areas.
+```
+- Line 28 (Change to your spotify client id)
+- Line 29 (Change to your spotify client secret)
+- Line 193 (Change to the prefix of your jingles to filter them out of the data)
+```
+--
+## Step 4) Running
+### The file can be ran on Windowsusing either [Visual Studio Code IDE](https://code.visualstudio.com/) or [Python for Windows](https://apps.microsoft.com/store/detail/python-310/9PJPW5LDXLZ5). 
+### To run this file on Linux or Mac, please view alternative guides.
+<br></br>
+
+## Contact Me
+If you're having issues running this application, let me know!
+- Discord: [sirnoob_1#0001](https://discord.com/users/304333708599754753)
---
+++ SpotifyMediaCompiler/SpotifyMediaCompiler.py
@@ -0,0 +1,250 @@
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ❯ SpotifyMediaCompiler ❮ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+'''
+   _____       _ _            _____           _ _               _____ _____
+  / ____|     (_) |          |  __ \         | (_)        /\   |  __ \_   _|
+ | (___   __ _ _| | ___  _ __| |__) |__ _  __| |_  ___   /  \  | |__) || |
+  \___ \ / _` | | |/ _ \| '__|  _  // _` |/ _` | |/ _ \ / /\ \ |  ___/ | |
+  ____) | (_| | | | (_) | |  | | \ \ (_| | (_| | | (_) / ____ \| |    _| |_
+ |_____/ \__,_|_|_|\___/|_|  |_|  \_\__,_|\__,_|_|\___/_/    \_\_|   |_____|
+'''
+
+#━━━━━━━━━━━━━━━━━━━━━━━ ❯ SpotifyMediaCompiler Imports ❮ ━━━━━━━━━━━━━━━━━━━━━━━
+
+#-Module Imports-
+import spotipy
+import requests
+import time
+import datetime
+
+#-Class Imports-
+from spotipy.oauth2 import SpotifyClientCredentials
+
+#━━━━━━━━━━━━━━━━━━━━━ ❯ SpotifyMediaCompiler Declerations ❮ ━━━━━━━━━━━━━━━━━━━━━
+
+#-Spotipy-
+spotifyClient = spotipy.Spotify(
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id='CLIENTID',
+        client_secret='CLIENTSECRET'
+        )
+    )
+
+#-Variables-
+responseList = []
+
+#━━━━━━━━━━━━ ❯ Functions ❮ ━━━━━━━━━━━
+
+#-Get_Api_Response-
+def get_filtered_api_response(songArtist: str, songTitle: str) -> dict or list:
+
+    #-Vars-
+    badCharacters = ['(', ')', '\'', '&', '.']
+    songTitleNew = ''
+    strWordCounter = 0
+    startTime = time.time()
+
+    #-Sequence-
+
+    for badChar in badCharacters: #- Iterating through the characters the API can't accept and removing them-
+
+        if badChar == '&':
+            songTitle = songTitle.replace('&', ', ')
+            songArtist = songArtist.replace(' & ', ', ')
+            continue
+
+        songTitle = songTitle.replace(badChar, '')
+        songArtist = songArtist.replace(badChar, '')
+
+    for strWord in songTitle.split(' '):#- Checking for 'feat' - Karaoke results-
+
+        if 'feat' in strWord.lower():
+            break
+
+        strWordCounter += 1
+
+        if strWordCounter == 1:
+            songTitleNew = strWord
+            continue
+
+        songTitleNew += ' ' + strWord
+
+    songTitle = songTitleNew
+
+    searchResults = spotifyClient.search( #- Searching the API-
+        q = 'artist: ' + songArtist.lower()  + ' track:' + songTitle.lower(),
+        type = 'track',
+        limit = 1
+    )
+
+
+    finishTime = time.time() - startTime
+
+    if searchResults['tracks']['items'] == []: #- Checking for a valid response, if it is a poor response this list is returned
+        print(f'({datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")}) [INFO] [GFAR] [FAILED] [{round(finishTime, 3)}s] Query ({songArtist} - {songTitle}) [RESPONSE] (Failed to locate song matching query)')
+
+        return [songArtist, songTitle, 'https://i.imgur.com/aEZDHey.png', None]
+
+    else:
+        print(f'({datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")}) [INFO] [GFAR] [SUCCESS] [{round(finishTime, 3)}s] Query ({songArtist} - {songTitle}) [RESPONSE] (' + searchResults['tracks']['items'][0]['external_urls']['spotify'] + ')')
+
+        return searchResults
+
+
+#-Fill_Response_list-
+def fill_response_list():
+    global responseList
+
+    #-Vars-
+    tempList = [None, None, None, None, None, None]
+    artistStr = ''
+    artistStrCounter = 0
+    songHistoryArtistStr = ''
+    songHistoryCounter = 1
+    songHistoryArtistStrCounter = 0
+    startTime = time.time()
+
+
+    #-Fetching SailorRadio API's Song Data-
+    stationData = requests.get('https://oreo.truckstopradio.co.uk/api/nowplaying/1')
+    stationData = stationData.json()
+
+    #-Searching Spotify API for song
+    searchResultsNP = get_filtered_api_response(stationData['now_playing']['song']['artist'], stationData['now_playing']['song']['title'])
+
+    if type(searchResultsNP) == dict:
+
+        #-Formatting artists to be presented-
+        for ResponseArtist in searchResultsNP['tracks']['items'][0]['artists']:
+
+            if artistStrCounter == 0:
+                artistStr += ResponseArtist['name']
+                artistStrCounter += 1
+            else:
+                artistStr += ', ' + ResponseArtist['name']
+
+        tempList[0] = [
+            artistStr,
+            searchResultsNP['tracks']['items'][0]['name'],
+            searchResultsNP['tracks']['items'][0]['album']['images'][0]['url'],
+            searchResultsNP['tracks']['items'][0]['external_urls']['spotify']
+        ]
+
+    else:
+        tempList[0] = searchResultsNP
+
+
+    for song in stationData['song_history']:
+        searchResults = get_filtered_api_response(song['song']['artist'], song['song']['title'])
+        songHistoryArtistStr = ''
+        songHistoryArtistStrCounter = 0
+
+        if type(searchResults) == dict:
+
+            for songAuthor in searchResults['tracks']['items'][0]['artists']:
+
+                if songHistoryArtistStrCounter == 0:
+                    songHistoryArtistStr += songAuthor['name']
+                    songHistoryArtistStrCounter += 1
+                else:
+                    songHistoryArtistStr += ', ' + songAuthor['name']
+                songHistoryArtistStrCounter += 1
+
+            tempList[songHistoryCounter] = [
+                songHistoryArtistStr,
+                searchResults['tracks']['items'][0]['name'],
+                searchResults['tracks']['items'][0]['album']['images'][0]['url'],
+                searchResults['tracks']['items'][0]['external_urls']['spotify']
+            ]
+
+        else:
+            tempList[songHistoryCounter] = searchResults
+
+        songHistoryCounter += 1
+        time.sleep(0)
+
+    finishTime = time.time() - startTime
+    print(f'({datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")}) [INFO] [FRL] [SUCCESS] [{round(finishTime, 3)}s] Filled ResponseList successfully')
+    responseList = tempList
+
+#-Update_Response_Data-
+def update_response_data():
+    global responseList
+
+
+    if responseList == []: #- Checking if the responselist dict is empty, and filling it if it is
+
+        fill_response_list()
+        return
+
+    #-Vars-
+    tempList = [[]]
+    fullList = responseList
+    artistStr = ''
+    artistStrCounter = 0
+    iterCounter = 0
+    startTime = time.time()
+
+    #-Fetching SailorRadio API's Song Data-
+    stationData = requests.get('STATIONLINK')
+    stationData = stationData.json()
+
+    #-Catching jingles-
+    if 'JINGLE PREFIX'.lower() in stationData['now_playing']['song']['title'].lower():
+        finishTime = time.time() - startTime
+        return print(f'[INFO] [URD] [FAILED - NORMAL] [{round(finishTime, 3)}s] Jingle detected when calling update_response_data. Function returned')
+
+    #-Searching Spotify API for song
+    searchResultsNP = get_filtered_api_response(stationData['now_playing']['song']['artist'], stationData['now_playing']['song']['title'])
+
+    if type(searchResultsNP) == dict:
+
+        #-Formatting artists to be presented-
+        for ResponseArtist in searchResultsNP['tracks']['items'][0]['artists']:
+
+            if artistStrCounter == 0:
+                artistStr += ResponseArtist['name']
+                artistStrCounter += 1
+            else:
+                artistStr += ', ' + ResponseArtist['name']
+
+        tempList[0] = [
+            artistStr,
+            searchResultsNP['tracks']['items'][0]['name'],
+            searchResultsNP['tracks']['items'][0]['album']['images'][0]['url'],
+            searchResultsNP['tracks']['items'][0]['external_urls']['spotify']
+        ]
+
+    else:
+        tempList[0] = searchResultsNP
+
+    if tempList[0] == responseList[0]:
+        finishTime = time.time() - startTime
+        print(f'({datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")}) [INFO] [URD] [FAILED] [{round(finishTime, 3)}s] Failed to update ResponseList due to the current song matching')
+
+        return
+
+
+    for item in fullList:
+
+        tempList.append(item)
+
+        iterCounter +=1
+        if iterCounter == 6:
+            break
+
+    finishTime = time.time() - startTime
+    responseList = tempList
+
+    print(f'({datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")}) [INFO] [URD] [SUCCESS] [{round(finishTime, 3)}s] Updated ResponseList succesfully')
+    return(responseList)
+
+update_response_data()
+
+'''
+Call update_response_data() to automatically update the 'cache' and to futurely update times. The function handles
+jingles and maintains the old list when azura updates the nowplaying with a jingle
+
+If and when the webserver restarts the jingles may show if a presenter is live, but this will go within 25 minutes and work as intended..
+Not a bug worth fixing'''
+
